7d1a2d9ef3a28c5be52a1778d7f76af0d63c8f80
Norbert Skurnog <norbert.skurnog@gmail.com>
2014-11-25 23:17:17 +0100

    Grammar: -  and needed again

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ parser = Grammar(r"""
 
 @start: expression ;
 
-@expression: function_call | cond_expr | loop | control_instr | print | return | arytm_expression | function_def | assign | declaration ;
+@expression: function_call | cond_expr | loop | control_instr | print | return | arytm_expression | function_def | assign_c | declaration ;
 
 type: 'int' | 'float' | 'string' ;
 if: 'if ' ;
@@ -22,10 +22,11 @@ number: '\d+' ;
 value: number | '"\w+"' ;
 
 control_instr: ('break' | 'continue') expression? ;
-return: 'return' (value | identifier | function_call | arytm_expression) expression? ;
-print: 'print' (value | identifier | function_call | arytm_expression )+ expression? ;
+return: 'return' (value | identifier | function_call | arytm_expression) ';' expression? ;
+print: 'print' (value | identifier | function_call | arytm_expression )+ ';' expression? ;
 
 assign: identifier '=' (function_call | value | identifier | arytm_expression ) expression? ;
+assign_c: assign ';' ;
 code_block: '\{' (expression)* '\}' ;
 
 parameter_list: '\(' variable? (',' variable)* '\)' ;
@@ -40,7 +41,7 @@ function_def: type identifier parameter_list code_block expression?;
 
 variable: type identifier ;
 
-declaration: type (assign | identifier) (',' (assign | identifier))* expression? ;
+declaration: type (assign | identifier) (',' (assign | identifier))* ';' expression? ;
 
 SPACES: '[ ]+' (%ignore) ;
 WS: '[ \t\n]+' (%ignore) (%newline);
@@ -48,32 +49,26 @@ WS: '[ \t\n]+' (%ignore) (%newline);
 """)
 
 result = parser.parse("""
-float a = 0, b = 0, c = 0
+float a = 0, b = 0, c = 0;
 
 int gcd(int m, int n) {
 
-int res = 0
+int res = 0;
 if (m!=n) {
     if (m > n)
-        res = gcd(m+n, n)
+        res = gcd(m+n, n);
     else
-        res = gcd(n+m, m)
-        xD =dd
-        int kurwa = tu, powinno, rzucac, bledem
-        lol = w
-        sumie = to
-        nie = xD
+        res = gcd(n+m, m);
 }
-else{
-    res = m
-}
-print res
-return res
+else
+    res = m;
+
+print res;
+return res;
 }
 
 while(a >= b ) {
-    a = 12*(a+ba)
+    a = 12*(a+ba);
 }
-
 """)
 print result
