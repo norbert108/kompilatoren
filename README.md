547ccaad81472d7980bc9996d546cb111a3bd7e9
Norbert Skurnog <norbert.skurnog@gmail.com>
2014-11-25 22:54:37 +0100

    Grammar: -  bug fixing

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Zalozenia:
 - nie obslugujemy prototypow funkcji, po zadeklarowaniu funkcji od razu nalezy zdefiniowac jej cialo
 - nie obslugujemy dziwnych rzeczy typu parametry domyslne (mozna dosc latwo zrobic ale po co xD)
 - nie obslugujemy else-if (nie ma podane w wymaganiach a problemy z parsowaniem jakies mam...)
+
+Co nie bangla:
+- sredniki przy przypisaniach (pojedynczych)
+- minus i srednik do dzielenia
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ parser = Grammar(r"""
 
 @start: expression ;
 
-@expression: function_call | cond_expr | loop | control_instr | print | declaration ;
+@expression: function_call | cond_expr | loop | control_instr | print | return | arytm_expression | function_def | assign | declaration ;
 
 type: 'int' | 'float' | 'string' ;
 if: 'if ' ;
@@ -14,35 +14,65 @@ else: 'else' ;
 while: 'while' ;
 repeat: 'repeat' ;
 until: 'until' ;
-number: '\d+' ;
 rel_operator: '<' | '>' | '<=' | '>=' | '==' | '!=' ;
 bin_operator: '\|\|' | '\&\&' ;
+arytm_operator:  '\+' | '\*'  ;
 identifier: '\w+' ;
+number: '\d+' ;
 value: number | '"\w+"' ;
-control_instr: ('return' | 'break' | 'continue') ';' expression? ;
+control_instr: ('break' | 'continue') ';' expression? ;
+return: 'return' (value | identifier | function_call ) ';' expression? ;
 print: 'print' (value | identifier | function_call )+ ';' expression? ;
 
-assign: identifier '=' value | variable '=' value ;
+assign: identifier '=' (function_call | value | identifier | arytm_expression ) expression? ;
 code_block: '\{' (expression)* '\}' ;
 
 parameter_list: '\(' variable? (',' variable)* '\)' ;
-arguments_list: '\(' (value)* '\)' ;
+arguments_list: '\(' (value | arytm_expression | identifier)? (',' (value | arytm_expression | identifier))* '\)' ;
 condition: '\(' ( identifier | identifier rel_operator identifier ) (bin_operator condition)* '\)' ;
 
-cond_expr: if condition code_block (else condition code_block)? expression? ;
+arytm_expression: (((number | identifier | arytm_expression) arytm_operator (number | identifier | arytm_expression)+ ) | '\(' arytm_expression '\)' ) expression? ;
+cond_expr: if condition (code_block | expression) (else (code_block | expression) )? expression? ;
 loop: (while condition code_block | repeat code_block until condition) expression? ;
 function_call: identifier arguments_list ';' expression? ;
-function_def: type identifier parameter_list code_block ;
+function_def: type identifier parameter_list code_block expression?;
 
 variable: type identifier ;
 
-declaration: (function_def | (type (identifier | assign) (',' (identifier | assign))* ';')) expression? ;
+declaration: type (assign | identifier) (',' (assign | identifier))* ';' expression? ;
 
 SPACES: '[ ]+' (%ignore) ;
 WS: '[ \t\n]+' (%ignore) (%newline);
 
 """)
 
-result = parser.parse("""int i = 43;
+result = parser.parse("""
+float a = 0, b = 0, c = 0;
+
+int gcd(int m, int n) {
+
+int res = 0;
+if (m!=n) {
+    if (m > n)
+        res = gcd(m+n, n);
+    else
+        res = gcd(n+m, m);
+        xD =dd
+        int kurwa = tu, powinno, rzucac, bledem ;
+        lol = w
+        sumie = to
+        nie = xD
+}
+else{
+    res = m
+}
+print res;
+return res;
+}
+
+while(a >= b ) {
+    a = 12*(a+ba)
+}
+
 """)
 print result
