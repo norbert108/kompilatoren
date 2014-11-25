dbb93ca494273fe3dade234fa9fd8bee4c27e753
Norbert Skurnog <norbert.skurnog@gmail.com>
2014-11-25 23:29:55 +0100

    Grammar: -  tree

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ parser = Grammar(r"""
 
 @expression: function_call | cond_expr | loop | control_instr | print | return | arytm_expression | function_def | assign_c | declaration ;
 
+
 type: 'int' | 'float' | 'string' ;
 if: 'if ' ;
 else: 'else' ;
 while: 'while' ;
 repeat: 'repeat' ;
 until: 'until' ;
+arytm_operator: '\+' | '\*' ;
 rel_operator: '<' | '>' | '<=' | '>=' | '==' | '!=' ;
 bin_operator: '\|\|' | '\&\&' ;
-arytm_operator:  '\+' | '\*'  ;
 identifier: '\w+' ;
+
 number: '\d+' ;
 value: number | '"\w+"' ;
 
@@ -31,7 +33,7 @@ code_block: '\{' (expression)* '\}' ;
 
 parameter_list: '\(' variable? (',' variable)* '\)' ;
 arguments_list: '\(' (value | arytm_expression | identifier)? (',' (value | arytm_expression | identifier))* '\)' ;
-condition: '\(' ( identifier | identifier rel_operator identifier ) (bin_operator condition)* '\)' ;
+condition: '\(' ( identifier | identifier rel_operator identifier ) (bin_operator ( identifier | identifier rel_operator identifier ) | condition)* '\)' ;
 
 arytm_expression: (((number | identifier | arytm_expression) arytm_operator (number | identifier | arytm_expression)+ ) | '\(' arytm_expression '\)' ) expression? ;
 cond_expr: if condition (code_block | expression) (else (code_block | expression) )? expression? ;
@@ -54,7 +56,7 @@ float a = 0, b = 0, c = 0;
 int gcd(int m, int n) {
 
 int res = 0;
-if (m!=n) {
+if (m!=n && a) {
     if (m > n)
         res = gcd(m+n, n);
     else
@@ -70,5 +72,5 @@ return res;
 while(a >= b ) {
     a = 12*(a+ba);
 }
-""")
+""").pretty()
 print result
